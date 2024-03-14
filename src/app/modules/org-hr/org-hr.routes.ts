eabf5ad87aa6c6b9d69f05f9563bf63f20102749
simran <sovesiyasimran@gmail.com>
2024-03-14 15:35:58 +0530

    created design for employee list and add form with excel file upload feature

--- src/app/modules/org-hr/org-hr.routes.ts
+++ src/app/modules/org-hr/org-hr.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { MembersComponent } from "../../shared/components/members/members.component";
 import { AddMemberComponent } from "../../shared/components/add-member/add-member.component";
+import { EmployeesComponent } from "../../shared/components/employees/employees.component";
+import { AddEmployeeComponent } from "../../shared/components/add-employee/add-employee.component";
 
 export const HR_ROUTES:Routes = [
     {
@@ -8,6 +10,12 @@ export const HR_ROUTES:Routes = [
     },
     {
         path:'member/:param',component:AddMemberComponent
+    },
+    {
+        path:'employee',component:EmployeesComponent
+    },
+    {
+        path:'employee/:param',component:AddEmployeeComponent
     },
     {
         path:'', redirectTo:'members',pathMatch:'full'
